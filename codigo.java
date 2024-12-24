ce41e460d9a2a5be21bb7902f0a7f6bb1b3c5b0e
Kylan021 <148078558+Kylan021@users.noreply.github.com>
2024-12-23 21:16:25 -0500

    Subiendo el archivo de Java

---
+++ codigo.java
@@ -0,0 +1,13 @@
+public class codigo{
+
+    public static void main(String[] args) {
+            
+        String nombre = "Andres";
+        String apellido = "Torres";
+
+        String saludo = "Hola "+nombre+" "+apellido+" ¿Como estas?";
+        System.out.println(saludo);
+
+    }
+
+}
