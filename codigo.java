902bce3618460713e326c024210cafa0ec33ebd0
Kylan021 <148078558+Kylan021@users.noreply.github.com>
2024-12-27 23:23:02 -0500

    Poniendo mi sef codigo.java

--- codigo.java
+++ codigo.java
@@ -2,7 +2,7 @@ public class codigo{
 
     public static void main(String[] args) {
             
-        String nombre = "Camila";
+        String nombre = "Andres Felipe";
         String apellido = "Torres";
 
         String saludo = "Hola "+nombre+" "+apellido+" ¿Como estas?";
